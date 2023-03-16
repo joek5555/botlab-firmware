6cf2d8b3e59d45670c4bd095602e7a74795ba62f
nocon <nocon@umich.edu>
2023-03-16 12:23:58 -0400

    gyrodometry complete!

--- src/mbot.c
+++ src/mbot.c
@@ -43,6 +43,7 @@ float global_left_duty = 0.0;
 float global_right_duty = 0.0;
 float global_left_velocity = 0.0;
 float global_right_velocity = 0.0;
+float prev_theta_gyro = 0.0;
 
 void timestamp_cb(timestamp_t *received_timestamp)
 {
@@ -195,16 +196,27 @@ bool timer_cb(repeating_timer_t *rt)
         float delta_d, delta_theta; // displacement in meters and rotation in radians
         delta_theta = (enc_delta_r*enc2meters - enc_delta_l*enc2meters)/WHEEL_BASE;
         delta_d = (enc_delta_r*enc2meters + enc_delta_l*enc2meters)/2;
-        current_odom.x = current_odom.x + delta_d * cos(current_odom.theta + delta_theta/2);
-        current_odom.y = current_odom.y + delta_d * sin(current_odom.theta + delta_theta/2);
-        current_odom.theta = current_odom.theta + delta_theta;
+        
+        //Get IMU data
+        float curr_theta_gyro = mpu_data.fused_TaitBryan[3];
+        float delta_theta_gyro = curr_theta_gyro - prev_theta_gyro;
+        prev_theta_gyro = curr_theta_gyro;
+        //Gyrodometry to update x,y,theta
+        if (fabsf(delta_theta_gyro - delta_theta) >= 0.01) {
+            current_odom.x = current_odom.x + delta_d * cos(current_odom.theta + delta_theta_gyro/2);
+            current_odom.y = current_odom.y + delta_d * sin(current_odom.theta + delta_theta_gyro/2);
+            current_odom.theta = current_odom.theta + delta_theta_gyro;
+        } else {
+            current_odom.x = current_odom.x + delta_d * cos(current_odom.theta + delta_theta/2);
+            current_odom.y = current_odom.y + delta_d * sin(current_odom.theta + delta_theta/2);
+            current_odom.theta = current_odom.theta + delta_theta;
+        }
         //clamp theta to between 0 and 2pi
         current_odom.theta = fmod(current_odom.theta, 2*PI);
         if (current_odom.theta < 0) {
             current_odom.theta = current_odom.theta + 2*PI;
         }
         current_odom.utime = cur_pico_time;
-
         /*************************************************************
          * End of TODO
          *************************************************************/
@@ -538,8 +550,8 @@ int main()
 
     while (running)
     {
-        //printf("\033[2A\r|      SENSORS      |           ODOMETRY          |     SETPOINTS     |\n\r|  L_ENC  |  R_ENC  |    X    |    Y    |    θ    |   FWD   |   ANG   \n\r|%7lld  |%7lld  |%7.3f  |%7.3f  |%7.3f  |%7.3f  |%7.3f  |", current_encoders.leftticks, current_encoders.rightticks, current_odom.x, current_odom.y, current_odom.theta, current_cmd.trans_v, current_cmd.angular_v);
-        printf("\033[2A\r|  l_cal  |  r_cal  | l_speed | r_speed| l_error |   l_pid  |   l_cmd | r_error  |   r_pid |  r_cmd   |\n\r|%7.3f   |%7.3f   |%7.3f  |%7.3f | %7.3f  |%7.3f  |%7.3f  |%7.3f  |%7.3f |%7.3f  |", global_left_calibration, global_right_calibration, global_left_velocity, global_right_velocity, global_left_error, global_left_pid_delta, global_left_duty, global_right_error, global_right_pid_delta, global_right_duty);
+        printf("\033[2A\r|      SENSORS      |           ODOMETRY          |     SETPOINTS     |\n\r|  L_ENC  |  R_ENC  |    X    |    Y    |    θ    |   FWD   |   ANG   \n\r|%7lld  |%7lld  |%7.3f  |%7.3f  |%7.3f  |%7.3f  |%7.3f  |", current_encoders.leftticks, current_encoders.rightticks, current_odom.x, current_odom.y, current_odom.theta, current_cmd.trans_v, current_cmd.angular_v);
+        //printf("\033[2A\r|  l_cal  |  r_cal  | l_speed | r_speed| l_error |   l_pid  |   l_cmd | r_error  |   r_pid |  r_cmd   |\n\r|%7.3f   |%7.3f   |%7.3f  |%7.3f | %7.3f  |%7.3f  |%7.3f  |%7.3f  |%7.3f |%7.3f  |", global_left_calibration, global_right_calibration, global_left_velocity, global_right_velocity, global_left_error, global_left_pid_delta, global_left_duty, global_right_error, global_right_pid_delta, global_right_duty);
     }
 }
 
